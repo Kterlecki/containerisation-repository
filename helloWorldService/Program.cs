805f29379371ee493eec7e3e430537ae7976bf61
Kterlecki <terlecki1995@gmail.com>
2024-06-03 17:51:47 +0100

    Add get call to receive values from backend

--- helloWorldService/Program.cs
+++ helloWorldService/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net.Http;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var _httpClient = new HttpClient();
+var response = await _httpClient.GetAsync("https://localhost:7096/WeatherForecast");
+var content = await response.Content.ReadAsStringAsync();
+Console.WriteLine(content);
